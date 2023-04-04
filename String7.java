5c0102048e20438e023b603f1a806ee1428d09f9
Srija444 <127313392+Srija444@users.noreply.github.com>
2023-04-03 22:33:58 -0700

    Add files via upload

---
+++ String7.java
@@ -0,0 +1,10 @@
+class String7 {
+  public static void main(String args[]) {
+
+    String str = "Hi hello welcome to bitlabs"; // no. of words
+    String words[] = str.split(" ");
+    // System.out.println(words[3]);
+    System.out.println("Number of words: " + words.length);
+
+  }
+}
